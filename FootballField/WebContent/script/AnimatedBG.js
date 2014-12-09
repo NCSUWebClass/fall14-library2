c9f1186e83c83f9bec2c5c7832e1b33668bbc4b8
John Mitchell <jlmitch5@ncsu.edu>
2014-12-08 21:23:17 -0500

    Adding second sinusoidal function to create waves

--- FootballField/WebContent/script/AnimatedBG.js
+++ FootballField/WebContent/script/AnimatedBG.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var canvas = $("#animated_sea")[0];
+	  var canvas = $("#animated_sea")[0];
 	  var ctx = canvas.getContext("2d");
 	  var MAX_HEIGHT = 3;
 	  
@@ -10,13 +10,13 @@ $(document).ready(function(){
 	  
 	  var dampen = function(x){
 	    x = x * Math.PI / 180;
-	    return MAX_HEIGHT /* Math.exp(-x / 4)*/ * Math.sin(x * 6);
+	    return (MAX_HEIGHT * Math.sin(x * 6));
 	  };
 	  
 	  for(var i = 0; i < heights.length; i++){
 	    heights[i] = dampen(-i);
 	  }
-	  
+
 	  var render = function( level ){
 	    ctx.clearRect(0, 0, canvas.width, canvas.height);
 	    ctx.beginPath();
@@ -33,7 +33,8 @@ $(document).ready(function(){
 	    ctx.fill();
 	  };
 	  
-	    var ctr = 0;
+	  var ctr = 0;
+
 	  var animate = function(fromLeft){
 	    setInterval(function(){
 	      if(fromLeft){
@@ -53,5 +54,55 @@ $(document).ready(function(){
 	    }, 23);
 	  };
 	  animate(true);
+
+	  var canvas = $("#animated_sea_2")[0];
+
+	  var ctx2 = canvas.getContext("2d");
+	  
+	  ctx2.fillStyle = "#CC0000";
+	  
+	  var heights2 = new Array(Math.floor(canvas.width / 2));
+	  
+	  
+	  var dampen2 = function(x){
+	    x = x * Math.PI / 180;
+	    return (MAX_HEIGHT * Math.sin(x * 4));
+	  };
+	  
+	  for(var i = 0; i < heights2.length; i++){
+	    heights2[i] = dampen2(-i);
+	  }
+
+	  var render2 = function( level ){
+	    ctx2.clearRect(0, 0, canvas.width, canvas.height);
+	    ctx2.beginPath();
+	    
+	    ctx2.moveTo(canvas.width, canvas.height);
+	    ctx2.lineTo(0, canvas.height);
+	    var dx = Math.round(canvas.width / heights2.length);
+	    var i = 0;
+	    for(; i < heights2.length - 1; i++){
+	      ctx2.lineTo(dx * i, canvas.height - (level + heights2[i]));
+	    }
+	    ctx2.lineTo(canvas.width, canvas.height - (level + heights2[i]));
+	    ctx2.closePath();
+	    ctx2.fill();
+	  };
+	  
+	  var ctr2 = 0;
+
+	  var animate2 = function(fromLeft){
+	    setInterval(function(){
+	      if(fromLeft){
+	        for(var i = 0; i < heights2.length - 1; i++){
+	          heights2[i] = heights2[i + 1];
+	        }
+	        heights2[heights2.length - 1] = dampen2(ctr2);
+	        ctr2 += 1;
+	      }
+	      render2(MAX_HEIGHT);
+	    }, 23);
+	  };
+	  animate2(true);
 
 });
